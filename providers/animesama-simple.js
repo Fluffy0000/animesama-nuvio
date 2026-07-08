fbb551fb642c7746fd350e41f4e6ad0d8a51a2fb
Fluffy0000 <Gabrieldudu992@gmail.com>
2026-07-09 01:30:27 +0200

    Refactor anime streaming functions for clarity

--- providers/animesama-simple.js
+++ providers/animesama-simple.js
@@ -2,122 +2,73 @@ var BASE_URL = "https://anime-sama.to";
 var UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36";
 var REF = BASE_URL + "/";
 
-function slugify(title) {
-  return title.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/['\u2019]/g, "").replace(/[^a-zA-Z0-9]+/g, "-").replace(/^-+|-+$/g, "").toLowerCase();
+function slugify(t) {
+  return t.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/['\u2019]/g, "").replace(/[^a-zA-Z0-9]+/g, "-").replace(/^-+|-+$/g, "").toLowerCase();
 }
 
-function getPlayerName(url) {
-  if (!url) return "Unknown";
-  var u = url.toLowerCase();
-  if (u.indexOf("sibnet") !== -1) return "Sibnet";
-  if (u.indexOf("vidmoly") !== -1) return "Vidmoly";
-  if (u.indexOf("sendvid") !== -1) return "Sendvid";
-  if (u.indexOf("smoothpre") !== -1) return "Smoothpre";
-  if (u.indexOf("oneupload") !== -1 || u.indexOf("uqload") !== -1) return "Uqload";
+function getPlayerName(u) {
+  if (!u) return "Unknown";
+  var l = u.toLowerCase();
+  if (l.indexOf("sibnet") !== -1) return "Sibnet";
+  if (l.indexOf("vidmoly") !== -1) return "Vidmoly";
+  if (l.indexOf("sendvid") !== -1) return "Sendvid";
+  if (l.indexOf("smoothpre") !== -1) return "Smoothpre";
+  if (l.indexOf("oneupload") !== -1 || l.indexOf("uqload") !== -1) return "Uqload";
   return "Player";
 }
 
-function parseEpisodesJs(jsContent) {
-  if (!jsContent) return [];
-  var results = [];
-  var varRegex = /var\s+([a-zA-Z0-9_]+)\s*=\s*\[([\s\S]*?)\]\s*;/g;
-  var match;
-  while ((match = varRegex.exec(jsContent)) !== null) {
-    var varName = match[1];
-    var urls = [];
-    var urlRegex = /['"](https?:\/\/[^'"]+)['"]/g;
-    var um;
-    while ((um = urlRegex.exec(match[2])) !== null) {
-      if (urls.indexOf(um[1]) === -1) urls.push(um[1]);
-    }
-    if (urls.length > 0) results.push({ varName: varName, urls: urls });
+function parseEpisodesJs(c) {
+  if (!c) return [];
+  var r = [], re = /var\s+([a-zA-Z0-9_]+)\s*=\s*\[([\s\S]*?)\]\s*;/g, m;
+  while ((m = re.exec(c)) !== null) {
+    var urls = [], ure = /['"](https?:\/\/[^'"]+)['"]/g, um;
+    while ((um = ure.exec(m[2])) !== null) { if (urls.indexOf(um[1]) === -1) urls.push(um[1]); }
+    if (urls.length > 0) r.push({ n: m[1], u: urls });
   }
-  return results;
+  return r;
 }
 
-function fetchEpisodesJs(slug, seasonPath, lang) {
-  var url = BASE_URL + "/catalogue/" + slug;
-  if (seasonPath) url += "/" + seasonPath;
-  url += "/" + lang + "/episodes.js";
-  return fetch(url, { headers: { "User-Agent": UA, "Referer": REF } })
-    .then(function(r) { return r.ok ? r.text() : null; })
-    .catch(function() { return null; });
+function fetchJs(s, p, l) {
+  var u = BASE_URL + "/catalogue/" + s;
+  if (p) u += "/" + p;
+  u += "/" + l + "/episodes.js";
+  return fetch(u, { headers: { "User-Agent": UA, "Referer": REF } }).then(function(r) { return r.ok ? r.text() : null; }).catch(function() { return null; });
 }
 
-function tryOneSlug(slug, season, episode) {
-  var languages = ["vostfr", "vf"];
-  var paths = ["saison" + season, ""];
-  return Promise.all(languages.map(function(lang) {
-    return Promise.all(paths.map(function(path) {
-      return fetchEpisodesJs(slug, path, lang).then(function(js) {
+function trySlug(s, sn, ep) {
+  var lgs = ["vostfr", "vf"], pts = ["saison" + sn, ""];
+  return Promise.all(lgs.map(function(lg) {
+    return Promise.all(pts.map(function(pt) {
+      return fetchJs(s, pt, lg).then(function(js) {
         if (!js) return [];
-        var parsed = parseEpisodesJs(js);
-        var streams = [];
-        for (var i = 0; i < parsed.length; i++) {
-          var arr = parsed[i];
-          if (arr.urls.length >= episode) {
-            var url = arr.urls[episode - 1];
-            if (url && url.indexOf("http") === 0) {
-              streams.push({
-                name: "Anime-Sama",
-                title: getPlayerName(url) + " - Ep " + episode + " - " + lang.toUpperCase(),
-                url: url,
-                quality: "HD",
-                headers: { "Referer": REF, "User-Agent": UA, "Origin": BASE_URL }
-              });
+        var pr = parseEpisodesJs(js), st = [];
+        for (var i = 0; i < pr.length; i++) {
+          if (pr[i].u.length >= ep) {
+            var u = pr[i].u[ep - 1];
+            if (u && u.indexOf("http") === 0) {
+              st.push({ name: "Anime-Sama", title: getPlayerName(u) + " - Ep " + ep + " - " + lg.toUpperCase(), url: u, quality: "HD", headers: { "Referer": REF, "User-Agent": UA } });
             }
           }
         }
-        return streams;
+        return st;
       });
     }));
-  })).then(function(results) {
-    var all = [];
-    for (var i = 0; i < results.length; i++) {
-      for (var j = 0; j < results[i].length; j++) {
-        all = all.concat(results[i][j]);
-      }
-    }
-    return all;
-  });
+  })).then(function(rr) { var a = []; for (var i = 0; i < rr.length; i++) for (var j = 0; j < rr[i].length; j++) a = a.concat(rr[i][j]); return a; });
 }
 
-function resolveSibnet(url) {
-  return fetch(url, { headers: { "User-Agent": UA, "Referer": REF } })
-    .then(function(r) { return r.text(); })
-    .then(function(html) {
-      // Find video src in the player page
-      var m = html.match(/src:\s*['"]([^'"]+)['"]/) 
-           || html.match(/file:\s*['"]([^'"]+)['"]/)
-           || html.match(/source\s+src=['"]([^'"]+)['"]/);
-      return m ? m[1] : url;
-    })
-    .catch(function() { return url; });
-}
-
-function getStreams(tmdbId, mediaType, season, episode) {
-  console.log("[Anime-Sama] " + mediaType + "/" + tmdbId + " S" + season + "E" + episode);
-  return fetch("https://api.themoviedb.org/3/" + mediaType + "/" + tmdbId + "?api_key=439c478a771f35c05022f9feabcca01c&language=en-US")
+function getStreams(id, type, sn, ep) {
+  console.log("[AS] " + type + "/" + id + " S" + sn + "E" + ep);
+  return fetch("https://api.themoviedb.org/3/" + type + "/" + id + "?api_key=439c478a771f35c05022f9feabcca01c&language=en-US")
     .then(function(r) { return r.json(); })
-    .then(function(data) {
-      var titles = [];
-      if (data.name) titles.push(data.name);
-      if (data.original_name && data.original_name !== data.name) titles.push(data.original_name);
-      var slugs = [];
-      for (var t = 0; t < titles.length; t++) {
-        var s = slugify(titles[t]);
-        if (s && slugs.indexOf(s) === -1) slugs.push(s);
-      }
-      function tryNext(index) {
-        if (index >= slugs.length) return [];
-        return tryOneSlug(slugs[index], season || 1, episode || 1).then(function(streams) {
-          if (streams.length > 0) return streams;
-          return tryNext(index + 1);
-        });
-      }
-      return tryNext(0);
-    })
-    .catch(function(e) { return []; });
+    .then(function(d) {
+      var tt = [];
+      if (d.name) tt.push(d.name);
+      if (d.original_name && d.original_name !== d.name) tt.push(d.original_name);
+      var ss = [];
+      for (var t = 0; t < tt.length; t++) { var sg = slugify(tt[t]); if (sg && ss.indexOf(sg) === -1) ss.push(sg); }
+      function nx(i) { if (i >= ss.length) return []; return trySlug(ss[i], sn || 1, ep || 1).then(function(st) { if (st.length > 0) return st; return nx(i + 1); }); }
+      return nx(0);
+    }).catch(function(e) { return []; });
 }
 
 module.exports = { getStreams: getStreams };
